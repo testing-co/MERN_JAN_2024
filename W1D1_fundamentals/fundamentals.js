907e200b1527c4d14af2c126ee9ea9beb71ace47
JupiterOrbita <38020888+jupiterorbita@users.noreply.github.com>
2024-01-08 11:08:14 -0700

    init commit

---
+++ W1D1_fundamentals/fundamentals.js
@@ -0,0 +1,30 @@
+// var name = "alice";
+// console.log(name); //?
+
+// function changeName() {
+//     name = "bob";
+// }
+
+// changeName();
+// console.log(name); //?
+
+// hoisting
+// const x = "bob"; // acts as an anchor
+// console.log(x);
+
+// x = "alice";
+// console.log(x);
+
+
+const arr = [11, 22, 33, 44];
+arr[0] = "bob";
+console.log(arr);
+
+const person = {
+    name: "bob",
+    favFood: "🍕"
+};
+
+person.name = "Fred";
+person.age = 22;
+console.log(person);

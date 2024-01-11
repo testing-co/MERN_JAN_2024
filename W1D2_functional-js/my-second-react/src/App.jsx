d8f27a469d18402a25e3267be1dd08b9098af520
JupiterOrbita <38020888+jupiterorbita@users.noreply.github.com>
2024-01-11 11:19:54 -0700

    add w1d2 second react app

---
+++ W1D2_functional-js/my-second-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <fieldset>
+    <legend>App.jsx</legend>
+    <h2>this is a div</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique excepturi sapiente nihil veniam facilis voluptas eius quia doloribus eveniet, fugit error totam? Sed nihil blanditiis, impedit voluptates, distinctio voluptate fuga doloremque assumenda amet temporibus odit earum illo. Enim laboriosam aliquid modi quo vitae deleniti ratione illum iure voluptas fugit, atque dignissimos est! Aut sequi, facere itaque quibusdam odit est odio?</p>;
+
+  </fieldset>;
+}
+
+export default App;
